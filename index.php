1d8580cb454f8d0e9e480a9b64717a6cb5b41fa1
matthewdss <matthewdss90@gmail.com>
2021-05-13 10:15:19 -0300

    adicionando index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROJETO FRONT</title>
+    <style>
+    .divblue{
+        background-color: blue;
+        border: solid 1px;
+        border-color: black;
+        width: 100%;
+        height: 400px;
+    }
+    </style>
+</head>
+<body>
+
+
+<div class="divblue">
+
+</div>
+
+<div>
+
+</div>
+    
+</body>
+</html>
